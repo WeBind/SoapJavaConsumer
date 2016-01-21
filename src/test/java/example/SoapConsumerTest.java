b381a74f61d768cc1e681a3cbd3c552406105e5e
MiguelJM <mikejm92@gmail.com>
2016-01-21 18:37:31 +0100

    Added test environment

---
+++ src/test/java/example/SoapConsumerTest.java
@@ -0,0 +1,201 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package example;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Adirael
+ */
+public class SoapConsumerTest extends TestCase {
+    
+    public SoapConsumerTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of run method, of class SoapConsumer.
+     *
+    public void testRun() throws Exception {
+        System.out.println("run");
+        SoapConsumer instance = null;
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class SoapConsumer.
+     */
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        //SoapConsumer.main(args);
+        
+    }
+
+    /**
+     * Test of getDuration method, of class SoapConsumer.
+     *
+    public void testGetDuration() {
+        System.out.println("getDuration");
+        SoapConsumer instance = null;
+        int expResult = 0;
+        int result = instance.getDuration();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDuration method, of class SoapConsumer.
+     *
+    public void testSetDuration() {
+        System.out.println("setDuration");
+        int duration = 0;
+        SoapConsumer instance = null;
+        instance.setDuration(duration);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPeriod method, of class SoapConsumer.
+     *
+    public void testGetPeriod() {
+        System.out.println("getPeriod");
+        SoapConsumer instance = null;
+        float expResult = 0.0F;
+        float result = instance.getPeriod();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPeriod method, of class SoapConsumer.
+     *
+    public void testSetPeriod() {
+        System.out.println("setPeriod");
+        int period = 0;
+        SoapConsumer instance = null;
+        instance.setPeriod(period);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getProvider method, of class SoapConsumer.
+     *
+    public void testGetProvider() {
+        System.out.println("getProvider");
+        SoapConsumer instance = null;
+        String expResult = "";
+        String result = instance.getProvider();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setProvider method, of class SoapConsumer.
+     *
+    public void testSetProvider() {
+        System.out.println("setProvider");
+        String provider = "";
+        SoapConsumer instance = null;
+        instance.setProvider(provider);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSize method, of class SoapConsumer.
+     *
+    public void testGetSize() {
+        System.out.println("getSize");
+        SoapConsumer instance = null;
+        int expResult = 0;
+        int result = instance.getSize();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSize method, of class SoapConsumer.
+     *
+    public void testSetSize() {
+        System.out.println("setSize");
+        int size = 0;
+        SoapConsumer instance = null;
+        instance.setSize(size);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStartingTime method, of class SoapConsumer.
+     *
+    public void testGetStartingTime() {
+        System.out.println("getStartingTime");
+        SoapConsumer instance = null;
+        int expResult = 0;
+        int result = instance.getStartingTime();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStartingTime method, of class SoapConsumer.
+     *
+    public void testSetStartingTime() {
+        System.out.println("setStartingTime");
+        int startingTime = 0;
+        SoapConsumer instance = null;
+        instance.setStartingTime(startingTime);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of putData method, of class SoapConsumer.
+     *
+    public void testPutData() {
+        System.out.println("putData");
+        String key = "";
+        Object value = null;
+        SoapConsumer instance = null;
+        instance.putData(key, value);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEndReceived method, of class SoapConsumer.
+     *
+    public void testSetEndReceived() {
+        System.out.println("setEndReceived");
+        Boolean endReceived = null;
+        SoapConsumer instance = null;
+        instance.setEndReceived(endReceived);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+//*/
+}
